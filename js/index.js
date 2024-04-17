48dee9f180d440675b4fe77fc01cfc3825c812ae
Michael Lowry <michael.lowry64@gmail.com>
2024-04-17 09:43:01 -0400

    did this entire thing in one go

---
+++ js/index.js
@@ -0,0 +1,67 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+getComputerChoice = () => choices[Math.floor(Math.random() * 3)];
+getHumanChoice = () => {
+    let choice = '';
+
+    // Ask user for input
+    choice = prompt("Rock, paper, or scissors?").toLowerCase();
+
+    // Keep asking until the input is valid
+    while (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors')
+        choice = prompt("Invalid input: Rock, paper, or scissors?").toLowerCase();
+
+    return choice;
+}
+
+function playRound(computerChoice, humanChoice) {
+
+    if (computerChoice == humanChoice){
+        console.log(`It's a tie! You both chose ${computerChoice}!`)
+        return;
+    }
+        
+    
+    switch(humanChoice) {
+        case 'rock':
+            switch(computerChoice) {
+                case 'paper':
+                    console.log("Computer chose paper. You lose!");
+                    break;
+                case 'scissors':
+                    console.log("Computer chose scissors. You win!");
+                    break;
+            }
+            break;
+        case 'paper':
+            switch(computerChoice) {
+                case 'rock':
+                    console.log("Computer chose rock. You win!");
+                    break;
+                case 'scissors':
+                    console.log("Computer chose scissors. You lose!");
+                    break;
+            }
+            break;
+        case 'scissors':
+            switch(computerChoice) {
+                case 'paper':
+                    console.log("Computer chose paper. You win!");
+                    break;
+                case 'rock':
+                    console.log("Computer chose rock. You lose!");
+                    break;
+            }
+            break;
+        default:
+            console.error("Something has gone very wrong.");
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getComputerChoice(), getHumanChoice());
+    }
+}
+
+playGame();
